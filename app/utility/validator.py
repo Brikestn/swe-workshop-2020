1a2436eab05db10be716dbde09a16ff9fedbc367
Brikestn <Brike123456789panyawit@gmail.com>
2020-09-06 13:44:10 +0700

    add validate name

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,4 +1,15 @@
+import re
+
+
 def validate_name(name):
+    digit = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    special = ["!", "@", "#", "$", "/", "<", ">", "-", "_", "?", "%", "^", "*"]
+    space = " "
+    if name == "":
+        return False
+    for char in name:
+        if char in digit or char in special or char in space:
+            return False
     return True
 
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,8 +3,23 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 
 class TestUtility(unittest.TestCase):
-    def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+    def test_validate_name_with_invalid_input_string_of_int(self):
+        self.assertEqual(False, validate_name("111111111"))
+
+    def test_validate_name_with_invalid_input_string_of_intstring(self):
+        self.assertEqual(False, validate_name("1denny50"))
+
+    def test_validate_name_with_valid_input_name(self):
+        self.assertEqual(True, validate_name("Anna"))
+
+    def test_validate_name_with_invalid_input_special_string(self):
+        self.assertEqual(False, validate_name("มิว##!!!"))
+
+    def test_validate_name_with_invalid_input_space(self):
+        self.assertEqual(False, validate_name("มิ   ว"))
+
+    def test_validate_name_with_invalid_noinput(self):
+        self.assertEqual(False, validate_name(""))
 
 
 if __name__ == '__main__':
